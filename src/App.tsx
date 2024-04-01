c917ac99cab5342f48a58d7d55677eecd5d0d4f7
wtricks <webtricks.ak@gmail.cmo>
2024-04-01 15:10:07 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider } from 'react-router-dom'
+import { Provider } from "react-redux";
+
+import { router } from './routes';
+import { store } from "./store";
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>Home page route: (-,-)</h1>
+    );
+};
+
+export default Home;
